d02c3fdc917854cf173f71b7d899f08c5778feb7
NC0312 <niketchawla983@gmail.com>
2025-01-07 23:31:13 +0530

    minor fix

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -4,13 +4,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-    };
+    // const settings = {
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 500,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    // };
 
     return (
         <div>
