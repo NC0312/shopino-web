909c9699d91e513fd1746496d6162fde8792452d
Jatin-1111 <off.jatin1111@gmail.com>
2025-01-15 11:26:50 +0530

    minor fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen bg-white overflow-hidden">
       {/* Announcement Bar */}
       <div className="bg-[#1D2564] text-white py-2 px-4">
         <div className="container mx-auto">
